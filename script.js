cfbda637c2ec897c3a8ea10ff018f99ba2e5e7fa
RahmaaEsmail <rahmaaesmail26@gmail.com>
2023-06-08 19:53:58 +0300

    update name of functions

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 'use strict'
 const numberAndOperationButtons = document.querySelectorAll('.btn');
-let currentOutputElement = document.querySelector('.current-output');
+const currentOutputElement = document.querySelector('.current-output');
 const equalButton = document.querySelector('.btn-equal');
 const clearAllButton = document.querySelector('.clear-all-btn');
 const deleteButton = document.querySelector('.btn-delete')
 
-const appendOperands = (data)=> {
+const appendOperands = (data) => {
     currentOutputElement.innerHTML += data;
 }
 
-const compute = ()=> {
+const performCalculation = () => {
     const result = eval(currentOutputElement.innerHTML)
     return result
 }
 
-const displayResult = ()=> {
+const displayResult = () => {
     currentOutputElement.innerHTML = compute()
 }
 
-const clearAll = () => {
+const clearAllOutput = () => {
     currentOutputElement.innerHTML = ''
 }
 
-const deleteChar = ()=> {
-    currentOutputElement.innerHTML = currentOutputElement.innerHTML.slice(0,-1)
+const deleteNumByNum = () => {
+    currentOutputElement.innerHTML = currentOutputElement.innerHTML.slice(0, -1)
 }
 
 
 
 
 numberAndOperationButtons.forEach(button => {
-    button.addEventListener('click',()=>{
-        if (button.innerHTML == '.' && currentOutputElement.innerHTML.includes('.') ) return;
+    button.addEventListener('click', () => {
+        if (button.innerHTML == '.' && currentOutputElement.innerHTML.includes('.')) return;
         appendOperands(button.innerHTML)
     })
 })
-equalButton.addEventListener('click',displayResult)
-clearAllButton.addEventListener('click',clearAll)
-deleteButton.addEventListener('click',deleteChar)
+equalButton.addEventListener('click', displayResult)
+clearAllButton.addEventListener('click', clearAll)
+deleteButton.addEventListener('click', deleteChar)
