700a3b41a73b1f049d105a1901c21059e4f1d010
RahmaaEsmail <rahmaaesmail26@gmail.com>
2023-06-15 02:29:01 +0300

    handle math error

--- script.js
+++ script.js
@@ -7,11 +7,25 @@ const deleteButton = document.querySelector('.btn-delete')
 
 const appendOperands = (data) => {
     currentOutputElement.innerHTML += data;
+    removeZeroFromStart(data)
+}
+
+const removeZeroFromStart = (str) => {
+
+    while (str[0] === '0') {
+        str = str.substring(1)
+    }
+    return str
 }
 
 const performCalculation = () => {
+    try{
     const result = eval(currentOutputElement.innerHTML)
     return result
+    }
+    catch {
+        return ("Math Error")
+    }
 }
 
 const displayResult = () => {
@@ -19,11 +33,16 @@ const displayResult = () => {
 }
 
 const clearAllOutput = () => {
-    currentOutputElement.innerHTML = ''
+        currentOutputElement.innerHTML = ''
 }
 
-const deleteNumByNum = () => {
+const deleteLastChar = () => {
+    if (performCalculation() == 'Math Error') {
+        currentOutputElement.innerHTML = ''
+    }
+    else {
     currentOutputElement.innerHTML = currentOutputElement.innerHTML.slice(0, -1)
+    }
 }
 
 
@@ -37,4 +56,4 @@ numberAndOperationButtons.forEach(button => {
 })
 equalButton.addEventListener('click', displayResult)
 clearAllButton.addEventListener('click', clearAllOutput)
-deleteButton.addEventListener('click', deleteNumByNum)
+deleteButton.addEventListener('click', deleteLastChar)
